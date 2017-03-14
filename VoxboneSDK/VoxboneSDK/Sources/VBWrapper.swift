5b3a8fc267a959f00e8c0d0964f559da113db4a8
Jerónimo Valli <jeronimo.valli@gmail.com>
2017-03-14 20:47:07 -0300

    Wrapper implemented: login & general call functions

--- VoxboneSDK/VoxboneSDK/Sources/VBWrapper.swift
+++ VoxboneSDK/VoxboneSDK/Sources/VBWrapper.swift
@@ -55,6 +55,28 @@ open class VBWrapper: NSObject {
         voxImplant.closeConnection()
     }
     
+    // MARK: - # Login
+    
+    open func login(withUsername user: String!, andPassword password: String!) {
+        voxImplant.login(withUsername: user, andPassword: password)
+    }
+    
+    open func login(withUsername user: String!, andOneTimeKey hash: String!) {
+        voxImplant.login(withUsername: user, andOneTimeKey: hash)
+    }
+    
+    open func login(withUsername user: String!, andToken token: String!) {
+        voxImplant.login(withUsername: user, andToken: token)
+    }
+    
+    open func refreshToken(withUsername user: String!, andToken token: String!) {
+        voxImplant.refreshToken(withUsername: user, andToken: token)
+    }
+    
+    open func requestOneTimeKey(withUsername user: String!) {
+        voxImplant.requestOneTimeKey(withUsername: user)
+    }
+    
     // MARK: - # Call
     
     open func createCall(_ to: String!, withVideo video: Bool, andCustomData customData: String!) -> String! {
@@ -82,6 +104,18 @@ open class VBWrapper: NSObject {
     }
     
     // MARK: - # General Call Actions
+    
+    open func sendDTMF(_ callId: String!, digit: Int32) {
+        voxImplant.sendDTMF(callId, digit: digit)
+    }
+    
+    open func sendMessage(_ callId: String!, withText text: String!, andHeaders headers: [AnyHashable : Any]!) {
+        voxImplant.sendMessage(callId, withText: text, andHeaders: headers)
+    }
+    
+    open func sendInfo(_ callId: String!, withType mimeType: String!, content: String!, andHeaders headers: [AnyHashable : Any]!) {
+        voxImplant.sendInfo(callId, withType: mimeType, content: content, andHeaders: headers)
+    }
     
     open func setMute(_ b: Bool) {
         voxImplant.setMute(b)
