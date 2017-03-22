1e00a06451cae96924fcf62f426699a0535b466b
Jerónimo Valli <jeronimo.valli@gmail.com>
2017-03-21 21:12:12 -0300

    Demo App: UI updates

--- VoxboneDemo/VoxboneDemo/Sources/ViewControllers/VDHomeVC.swift
+++ VoxboneDemo/VoxboneDemo/Sources/ViewControllers/VDHomeVC.swift
@@ -17,22 +17,33 @@ class VDHomeVC: UIViewController {
         super.viewDidLoad()
         
         navigationItem.hidesBackButton = true
+        
+        buttonCall.clipsToBounds = true
+        buttonCall.layer.cornerRadius = 5
     }
     
     @IBAction func onClickCall() {
-        //let callTo = "16502106894"
-        let callTo = "test2"//@voxbonedemo.voxboneworkshop.voximplant.com"
+        let callTo = "5492216093000"//"5492215585444"//"16502106894"
+        //let callTo = "test2"//@voxbonedemo.voxboneworkshop.voximplant.com"
         VDVoxboneManager.shared.call(to: callTo, 
             onCallConnected: { (VDOnCallConnectedHandler) in
-                
+                self.setButtonCallState(true)
             }, onCallDisconnected: { (VDOnCallDisconnectedHandler) in
-            
+                self.setButtonCallState(false)
             }, onCallRinging: { (VDOnCallRingingHandler) in
             
             }, onCallFailed: { (VDOnCallFailedHandler) in
-            
+                self.setButtonCallState(false)
             }, onCallAudioStarted: { (VDOnCallAudioStartedHandler) in
             
             })
+    }
+    
+    func setButtonCallState(_ isOnCall: Bool) {
+        if isOnCall {
+            buttonCall.setTitle("Hangup", for: .normal)
+        } else {
+            buttonCall.setTitle("Call", for: .normal)
+        }
     }
 }
--- VoxboneDemo/VoxboneDemo/Sources/ViewControllers/VDLoginVC.swift
+++ VoxboneDemo/VoxboneDemo/Sources/ViewControllers/VDLoginVC.swift
@@ -10,32 +10,50 @@ import UIKit
 
 class VDLoginVC: UIViewController {
     
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var buttonConnect: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setButtonConnectState(false)
+        
+        buttonConnect.clipsToBounds = true
+        buttonConnect.layer.cornerRadius = 5
+        
+        textFieldUsername.text = "test1@voxbonedemo.voxboneworkshop.voximplant.com"
+        textFieldPassword.text = "123456"
     }
 
     @IBAction func onClickConnect() {
+        if textFieldUsername.text != nil, textFieldUsername.text!.characters.count <= 0,
+            textFieldPassword.text != nil, textFieldPassword.text!.characters.count <= 0 {
+            print("Error: Username & Password is required!")
+            return
+        }
+        
         VDVoxboneManager.shared.connect(
-            onConnectionSuccessful: { (VDOnConnectionSuccessfulHandler) in
+            onConnectionSuccessful: {
                 self.login()
-            }, onConnectionFailed: { (VDOnConnectionFailedHandler) in
+            }, onConnectionFailed: { (_ error: Error) in
+                print("Error: \(error)")
                 self.setButtonConnectState(false)
-            }, onConnectionClosed: { (VDOnConnectionClosedHandler) in
+            }, onConnectionClosed: {
                 self.setButtonConnectState(false)
             })
     }
     
     func login() {
-        VDVoxboneManager.shared.login("test1@voxbonedemo.voxboneworkshop.voximplant.com", "123456",
-            onLoginSuccessful: { (VDOnLoginSuccessfulHandler) in
+        VDVoxboneManager.shared.login(textFieldUsername.text!, textFieldPassword.text!,
+            onLoginSuccessful: { (_ displayName: String, _ authParams: [AnyHashable : Any]) in
                 self.setButtonConnectState(true)
                 self.performSegue(withIdentifier: Constants.Segue.toHome, sender: nil)
-            }, onLoginFailed: { (VDOnLoginFailedHandler) in
-                self.setButtonConnectState(false)
+            }, onLoginFailed: { (_ errorCode: NSNumber) in
+                print("Error: \(errorCode)")
+                VDVoxboneManager.shared.close(onConnectionClosed: {
+                    self.setButtonConnectState(false)
+                })
             })
     }
     
